fa544418bfa0b9c775ee9a2b033424f398bee4a8
Lemon-tech24 <78329279+Lemon-tech24@users.noreply.github.com>
2022-11-03 22:15:50 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TODO | Alisangco</title>
+    <link rel="stylesheet" href="./style.css">
+
+</head>
+    
+<body>
+    <div class="title">TODO APP | Alisangco</div>
+    <div class="main-container">
+        
+
+        <form class="btn-box" method="POST">
+
+            <div class="todo-box">
+                <input type="text" class="task" name="task" placeholder="Write your Task" required>
+            </div>
+
+            <input type="submit" name="add" value="ADD" class="btns" id="add">
+            
+        </form>
+        <div class="add-box">
+            <?php include 'main.php'?>
+        </div>
+    
+      
+    </div>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,67 @@
+<?php
+ 
+    session_start();
+    $status="";
+
+    function printForm($value, $index){
+        echo "<form class=\"added-task\" method=\"POST\">
+                    <input type=\"hidden\" name=\"newtask\"  value=\"$value\">
+                    <input type=\"text\" value=\"$index: $value\" disabled>
+                    <input type=\"submit\" name=\"delete\" value=\"DELETE\" class=\"btns\">
+              </form>";          
+    }
+
+    
+    function uniqueForm($unique){
+        foreach($unique as $value){
+            if(!$value){continue;}
+            $index = array_search($value, $unique);
+            printForm($value, $index);
+        }
+    }
+
+
+    
+    if(isset($_POST['task']) && isset($_POST['add'])){
+        $task = $_POST['task'];
+        if(!empty($task)){
+            if (!isset($_SESSION['task'])) {
+                $_SESSION['tasks'][0]= "";
+            }
+                array_push($_SESSION['tasks'], $task);
+                $status = "Successfully Added";
+                $unique = array_values(array_unique($_SESSION['tasks']));
+                uniqueForm($unique);        
+        }    
+    }
+
+
+    if(isset($_POST['delete'])){
+        $unique = array_values(array_unique($_SESSION['tasks']));
+      
+        $newtask = $_POST['newtask'];
+        
+        $index = array_search($newtask, $unique);
+
+     
+        unset($unique[$index]);
+        $unique = array_values($unique);
+        $_SESSION['tasks'] = $unique;
+
+        uniqueForm($unique);
+    }
+
+
+    //session_unset();
+   
+  
+   
+
+   
+
+   
+
+  
+
+ 
+?>
